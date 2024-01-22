714947bd6211690511ed037fa965ac139a40e022
Rahul <rahulkrish321123@gmail.com>
2024-01-23 00:43:56 +0530

    fixed contact mail issue

--- Portfolio/src/Sections/Contact.jsx
+++ Portfolio/src/Sections/Contact.jsx
@@ -7,7 +7,8 @@ function Contact() {
         <div className={"Contact_content"}><p>I'm currently looking for any new opportunities, my inbox is always open.
             Whether you have a question or just want to say hello, I will provide a swift response! Feel free to mail me
             about any relevant job updates.</p></div>
-        <button className={"Resume_button _button"}>Mail me</button>
+        <button className={"Resume_button _button"}><a id={"contact_button"} style={{textDecoration: "None"}}
+                                                       href={"mailto:rahulkrish321123@gmail.com"}>Mail me!</a></button>
     </div>)
 }
 
