fb403e50eee949f0a4fed227e2e983157f9052f5
Rahul <rahulkrish321123@gmail.com>
2024-01-23 00:45:21 +0530

    Fixed contact email Section

--- Portfolio/src/Sections/Contact.jsx
+++ Portfolio/src/Sections/Contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
             Whether you have a question or just want to say hello, I will provide a swift response! Feel free to mail me
             about any relevant job updates.</p></div>
         <button className={"Resume_button _button"}><a id={"contact_button"} style={{textDecoration: "None"}}
-                                                       href={"mailto:rahulkrish321123@gmail.com"}>Mail me!</a></button>
+                                                       href={"mailto:rahulkrish321123@gmail.com"} >Mail me!</a></button>
     </div>)
 }
 
