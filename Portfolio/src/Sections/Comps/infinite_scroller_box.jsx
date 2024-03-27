26366b8284a319c1b02dd740ab0101a73bd42a44
Rahul <rahulkrish321123@gmail.com>
2024-03-27 18:58:28 +0530

    Reduced speed , hover pause and style change

--- Portfolio/src/Sections/Comps/infinite_scroller_box.jsx
+++ Portfolio/src/Sections/Comps/infinite_scroller_box.jsx
@@ -3,7 +3,7 @@
 export default function Infinite_box(props){
 
     return (
-        <span style={{display:"flex",justifyContent:"center", padding:"12px 25px",alignItems:"center",gap:"5px",border:"1px solid rgba(255, 255, 255, 0.82) ",color: "var(--green)",width:"max-content", borderRadius:"10px" }}>
+        <span className={"infinite_divs"} style={{display:"flex",justifyContent:"center", padding:"12px 25px",alignItems:"center",gap:"5px",width:"max-content", borderRadius:"10px" }}>
             <img style={{height:"20px",width:"20px",}} src={props.logo}/>
             <a style={{fontSize:"1.2rem",fontFamily: "var(--font-mono)"}}>{props.name}</a>
         </span>
