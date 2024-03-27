86a780e4c89604e3cfb57e9f63c2bd65935d47f6
Rahul <rahulkrish321123@gmail.com>
2024-03-27 19:22:04 +0530

    Changed font size

--- Portfolio/src/Sections/Comps/infinite_scroller_box.jsx
+++ Portfolio/src/Sections/Comps/infinite_scroller_box.jsx
@@ -5,7 +5,7 @@ export default function Infinite_box(props){
     return (
         <span className={"infinite_divs"} style={{display:"flex",justifyContent:"center", padding:"12px 25px",alignItems:"center",gap:"5px",width:"max-content", borderRadius:"10px" }}>
             <img style={{height:"20px",width:"20px",}} src={props.logo}/>
-            <a style={{fontSize:"1.2rem",fontFamily: "var(--font-mono)"}}>{props.name}</a>
+            <a style={{fontSize:"0.9rem",fontFamily: "var(--font-mono)"}}>{props.name}</a>
         </span>
     )
 }
