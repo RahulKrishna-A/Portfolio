dbde9060c90e5e87daba2ea892727f922054194c
RahulKrishna <rahulkrish321123@gmail.com>
2024-09-27 02:09:15 +0530

    Update Contact.jsx

--- Portfolio/src/Sections/Contact.jsx
+++ Portfolio/src/Sections/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
     return (<div id={"Contact_container"}>
         <p className={"Contact_title"}>05. What's next</p>
         <p className={"Contact_Desc"}>Get In Touch</p>
-        <div className={"Contact_content"}><p>I'm currently looking for any new opportunities, my inbox is always open.
+        <div className={"Contact_content"}><p>My inbox is always open.
             Whether you have a question or just want to say hello, I will provide a swift response! Feel free to mail me
             about any relevant job updates.</p></div>
         <button className={"Resume_button _button"}><a id={"contact_button"} style={{textDecoration: "None"}}
